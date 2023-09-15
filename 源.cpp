a3d0b881d335e2bd3da4076a0a34d7923d149ab2
嫩叠 我 <15659852382@163.com>
2023-09-15 21:26:11 +0800

    as

--- 源.cpp
+++ 源.cpp
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include	<stdio.h>
 #include	<assert.h>
 
 //——————求字符串长度——————————————————————————————
@@ -55,12 +55,125 @@ int Narcissistic_number(int i) {
 //	}
 //	return 0;
 //}
+//int main() {
+//	int money = 0;
+//	scanf("%d", &money);
+//	if (money>0)
+//		printf("%d\n", 2 * money - 1);
+//	else 
+//		printf("%d\n", 0);
+//	return 0;
+//}
+//————————————————————————————————————
+//倒置字符串
+void reverse(char* left, char* right) {
+	assert(left); assert(right);
+	while (left<right)
+	{
+		char tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++; right--;
+	}
+}
+//int main() {
+//	char arr[10000] = { 0 };
+//	//输入
+//	gets_s(arr);
+//	//倒置
+//	int len = my_strlen(arr);
+//	//总逆序
+//	reverse(arr,arr+len-1);
+//	//逆序个个单词
+//	char* start = arr;
+//	while (*start)
+//	{
+//		char* end = start;
+//		while (*end != ' ' && *end != '\0')
+//			end++;
+//		reverse(start, end - 1);
+//		if (*end != '\0')
+//			end++;
+//		start = end;
+//	}
+//	//输出
+//	printf("%s", arr);
+//	return 0;
+//}
+#include <Windows.h>
+//————————————————————————————————————
+
+//int main() {
+	/*unsigned int i;*/
+	//for (i = 3; i >= 0; i--)
+	//	//-1 -> unsignde int i ->0xffffffff
+	//	//-2 ->................->0xffffffff-1
+ //	{
+	//	printf("%u \n", i);
+	//	Sleep(500);//休眠5000ms
+	//}
+//return 0; }
+
+//int main() {
+	//char a = 127;
+	//a++;
+	//printf("%d\n", a);
+	//char b = a - 128;
+	//printf("%d\n", b);
+	//char c = a + 128;
+	//printf("%d\n", c);	
+//return 0; }
+
+//int main() {
+//	//任意二个二进制浮点数 V 可以表示成下面的形式：
+//	//V=(-1)^S * M * 2^E
+//	//(-1)^S表示符号位
+//	//M表示有效数字
+//	//2^E表示位数
+//	//V = 5.0f ;--二进制-->101.2--科学计数->1.10*2^2
+//	//  =(-1)^0 * 1.01 * 2^2
+//	//	  S=0	  M=1.01 E=2
+//	//V = 9.5f;
+//	//  =1001.1
+//	//  =(-1)^0 * 1.0011 * 2^3
+//	//	  S=0	 M=1.0011  E=3
+//	//V = 9.6f
+//	//  = 1001.100..1000010011010101010101010101010(无法精确)
+//	//V = 5.5f
+//	//  S = 0
+//	//  M = 0x40 b0 00 00
+//	//  E = 2+127/1023
+//	//对于 32 位的浮点数，
+//	//最高的 1 位是符号位 S(1bit)
+//	//接着的 8 位是指数 E(8/11bit)
+//	//剩下的 23 位为有效数字 M(23/52bit)
+//	//取出时
+//	//	E 不全为 0 或不全为 1
+//	//指数 E 的计算值减去 127 （或 1023 ），得到真实值，
+//	//再将有效数字 M 前加上第一位的 1 
+//}	// E 全为 0
+//	//这时，浮点数的指数 E 等于 1-127 （或者1-1023）即为真实值，
+//	//有效数字 M 不再加上第一位的 1 ，而是还原为 0.XXXXXXXXXXXXX 的小数。
+//	//这样做是为了表示± 0 ，以及接近于0 的很小的数字。	
+//	// E 全为 1
+//	//有效数字 M 全为 0 ，表示±无穷大（正负取决于符号位 s ）；
+	//int n = 9;
+	//float* pfloat = (float*)&n;
+	//printf("n2:%d\n", n);
+	////00000000 00000000 00000000 00001001
+	//printf("*pflota:%f\n", *pfloat);
+	////pfloat	0x0000009c17f3fc04 {1.261e-44#DEN}--->0	
+	//*pfloat = 9.0;
+	////S=0 E=3 M=1.001
+	////0x00 00 10 41
+	//printf("n1:%d\n", n);
+	////00 00 10 41
+	//printf("*pflota:%f\n", *pfloat);
+	//return 0;}
+
+//————————————————有序的序列合并————————————————————
+//
 int main() {
-	int money = 0;
-	scanf("%d", &money);
-	if (money>0)
-		printf("%d\n", 2 * money - 1);
-	else 
-		printf("%d\n", 0);
+	
 	return 0;
 }
